b5877884791f2feebef238bdfc1edad9d4c32b2b
agandia <aitor_gv@hotmail.com>
2025-06-19 13:46:53 +0200

    color sphere with normals using quadratic eq

--- Raytracer/Utilities.hpp
+++ Raytracer/Utilities.hpp
@@ -9,21 +9,28 @@ void write_color(std::ostream &out, const glm::vec3 color) {
         << static_cast<int>(255.999f * color.b) << '\n';
 }
 
-bool hit_sphere(const glm::dvec3& center, double radius, const Ray& ray) {
+double hit_sphere(const glm::dvec3& center, double radius, const Ray& ray) {
   glm::dvec3 oc = center - ray.origin(); //ray from center of sphere to ray origin
   double a = glm::dot(ray.direction(), ray.direction());
-  double b = 2.0 * glm::dot(oc, ray.direction());
+  double b = -2.0 * glm::dot(oc, ray.direction());
   double c = glm::dot(oc, oc) - radius * radius;
   double discriminant = b * b - 4 * a * c;
-  return (discriminant > 0);
+  
+  if(discriminant < 0) {
+    return -1.0; // No intersection
+  } else {
+    return (-b - glm::sqrt(discriminant)) / (2.0 * a); // Return the nearest intersection point
+  }
 }
 
 glm::vec3 ray_color(const Ray& ray) {
-  if(hit_sphere(glm::dvec3(0, 0, -1), 0.5, ray)) {
-    return glm::vec3(1.0f, 0.0f, 0.0f); // Red color for the sphere
+  const double hit_test = hit_sphere(glm::dvec3(0, 0, -1), 0.5, ray);
+  if(hit_test > 0.0) {
+    glm::vec3 normal = glm::normalize(ray.at(hit_test) - glm::dvec3(0, 0, -1));
+    return 0.5f * glm::vec3(normal.x + 1.0f, normal.y + 1.0f, normal.z + 1.0f); // Use normal to create a color
   }
 
   glm::dvec3 unit_direction = glm::normalize(ray.direction());
   float t = 0.5f * ((float)unit_direction.y + 1.0f); // Map y component to [0, 1]
-  return (1.0f - t) * glm::vec3(1.0f) + t * glm::vec3(0.5f, 0.7f, 1.0f); // Gradient from white to blue
+  return (1.0f - t) * glm::vec3(1.0f) + t * glm::vec3(0.5f, 0.7f, 1.0f); // Gradient from white to blue for the background
 }
